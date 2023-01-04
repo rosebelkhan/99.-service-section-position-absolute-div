03310253c138fc70e7267027b2c996b09bf95e0b
Rosebel Khan <mahady342@gmail.com>
2023-01-05 01:58:49 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+(function($){
+    "use strict";
+
+
+
+
+
+    $(document).ready(function(){
+
+        $('.homepage-slider').owlCarousel({
+              items:1,
+              loop: true,
+              nav: true,
+              dots: true,
+              autoplay: true,
+              dotsData: true,
+          });
+
+      });
+
+
+
+
+    }(jQuery));
+    
+    /* navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],*/ 
+
+
+
